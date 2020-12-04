70dbc73aabe4a58e238a3c0158b4fd2bcfe3c68e
JbIPS <jeanbaptiste.richardet@gmail.com>
2020-12-04 20:39:12 +0100

    Plouf

--- _pages/about.md
+++ _pages/about.md
@@ -1,13 +1,19 @@
 ---
 layout: about
+class: about
 title: Mes convictions
 author: Flora Richardet
 image: assets/images/uploads/profile.jpg
-signature: assets/images/about/signature.png
 button: Qui suis-je ?
 ---
+### Mon expérience
+
 Passionnée des arts graphiques depuis longtemps, j'ai toujours aimé peindre et créer des affiches et logos pour les associations et évènements locaux. Après une carrière en ingénérie, j'ai décidé de me lancer pleinement dans le monde du design pour aider les porteurs de projets à optimiser leur communication visuelle.
 
+### Mes convictions
+
 Mes dix années d'expérience de cheffe de projet m'ont appris à bien comprendre les attentes de mes partenaires, à bien gérer les priorités et à procéder par itérations afin d'être toujours alignée avec ce qu'ils avaient en tête.
+
+### Mes expérimentations
 
 Ma volonté est de mettre mes compétences graphiques aux services des projets qui résonnent avec mes convictions : écologie, utilité sociale et humanisme. J'aime travailler sur différent supports, qu'il s'agisse du web, du papier ou même du tissus. Vous avez une idée derrière la tête sans savoir comment la concrétiser ? Demandez-moi !
--- assets/js/script.js
+++ assets/js/script.js
@@ -37,40 +37,30 @@
     /* ########################################### hero parallax ############################################## */
     window.onload = function () {
         var parallaxBox = document.getElementById('parallax');
-        var
-            /* c1left = document.getElementById('l1').offsetLeft,
-                       c1top = document.getElementById('l1').offsetTop, */
-            c2left = document.getElementById('l2').offsetLeft,
-            c2top = document.getElementById('l2').offsetTop,
-            c3left = document.getElementById('l3').offsetLeft,
-            c3top = document.getElementById('l3').offsetTop,
-            c4left = document.getElementById('l4').offsetLeft,
-            c4top = document.getElementById('l4').offsetTop,
-            c5left = document.getElementById('l5').offsetLeft,
-            c5top = document.getElementById('l5').offsetTop,
-            c6left = document.getElementById('l6').offsetLeft,
-            c6top = document.getElementById('l6').offsetTop,
-            c7left = document.getElementById('l7').offsetLeft,
-            c7top = document.getElementById('l7').offsetTop,
-            c8left = document.getElementById('l8').offsetLeft,
-            c8top = document.getElementById('l8').offsetTop,
-            c9left = document.getElementById('l9').offsetLeft,
-            c9top = document.getElementById('l9').offsetTop;
+        const elements = [];
+        let i = 2;
+        let elemExist = true;
+        const offsets = [25, 20, 35, 30, 45, 30, 25, 40];
+        while(elemExist) {
+            const id = `l${i}`;
+            const el = document.getElementById(id);
+            i += 1;
+            if(!el) elemExist = false
+            else elements.push({
+                id,
+                left: el.offsetLeft,
+                top: el.offsetTop
+            });
+        }
 
         parallaxBox.onmousemove = function (event) {
             event = event || window.event;
             var x = event.clientX - parallaxBox.offsetLeft,
                 y = event.clientY - parallaxBox.offsetTop;
 
-            /*  mouseParallax('l1', c1left, c1top, x, y, 5); */
-            mouseParallax('l2', c2left, c2top, x, y, 25);
-            mouseParallax('l3', c3left, c3top, x, y, 20);
-            mouseParallax('l4', c4left, c4top, x, y, 35);
-            mouseParallax('l5', c5left, c5top, x, y, 30);
-            mouseParallax('l6', c6left, c6top, x, y, 45);
-            mouseParallax('l7', c7left, c7top, x, y, 30);
-            mouseParallax('l8', c8left, c8top, x, y, 25);
-            mouseParallax('l9', c9left, c9top, x, y, 40);
+            elements.forEach((el, index) => {
+                mouseParallax(el.id, el.left, el.top, x, y, offsets[index % offsets.length]);
+            })
         };
 
     };
