07f022e27b3446e706e4827217de38b3f9b397ab
JbIPS <JbIPS@users.noreply.github.com>
2021-02-11 21:59:12 +0000

    deploy: 01f2c6c92b61b9038f681b7a4b11a26226ec2787

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,41 @@
     'use strict';
 
     // Sticky Menu    
+    let scheduledFoldAF = false;
     const foldHeader = function () {
         const distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 100,
             header = document.querySelector(".navigation");
-        if (distanceY > shrinkOn) {
-            header.classList.add("nav-bg");
-        } else {
-            header.classList.remove("nav-bg");
-        }
+
+        if (scheduledFoldAF) return;
+
+        requestAnimationFrame(() => {
+            if (distanceY > shrinkOn) {
+                header.classList.add("nav-bg");
+            } else {
+                header.classList.remove("nav-bg");
+            }
+            scheduledFoldAF = false;
+        });
     };
 
     window.addEventListener("scroll", foldHeader);
-    window.addEventListener("load", foldHeader);
 
     /* ########################################### hero parallax ############################################## */
+    let scheduledHeroAF = false;
     const initParallax = function () {
         var parallaxBox = document.getElementById('parallax');
-        if(!parallaxBox) return;
+        if (!parallaxBox) return;
 
         const elements = [];
         let i = 2;
         let elemExist = true;
         const offsets = [25, 20, 35, 30, 45, 30, 25, 40];
-        while(elemExist) {
+        while (elemExist) {
             const id = `l${i}`;
             const el = document.getElementById(id);
             i += 1;
-            if(!el) elemExist = false
+            if (!el) elemExist = false
             else elements.push({
                 id,
                 left: el.offsetLeft,
@@ -37,40 +44,49 @@
             });
         }
 
-        parallaxBox.onmousemove = function (event) {
+        parallaxBox.addEventListener("mousemove", (event) => {
             event = event || window.event;
             var x = event.clientX - parallaxBox.offsetLeft,
                 y = event.clientY - parallaxBox.offsetTop;
 
-            elements.forEach((el, index) => {
-                mouseParallax(el.id, el.left, el.top, x, y, offsets[index % offsets.length]);
-            })
-        };
+            const moves = elements.map((el, index) => {
+                const speed = offsets[index % offsets.length];
+                const obj = document.getElementById(el.id);
+                const parentObj = obj.parentNode,
+                    containerWidth = parseInt(parentObj.offsetWidth),
+                    containerHeight = parseInt(parentObj.offsetHeight);
+                return {
+                    obj,
+                    left: ((x - (parseInt(obj.offsetWidth) / 2 + el.left)) / containerWidth) * speed,
+                    top: ((y - (parseInt(obj.offsetHeight) / 2 + el.top)) / containerHeight) * speed
+                };
+            });
+
+            if (scheduledHeroAF) return;
+            requestAnimationFrame(() => {
+                moves.forEach((move) => {
+                    move.obj.style.transform = `translate(${move.left}px, ${move.top}px)`;
+                });
+                scheduledHeroAF = false;
+            });
+        }, {
+            passive: true
+        });
 
     };
 
-    function mouseParallax(id, left, top, mouseX, mouseY, speed) {
-        var obj = document.getElementById(id);
-        var parentObj = obj.parentNode,
-            containerWidth = parseInt(parentObj.offsetWidth),
-            containerHeight = parseInt(parentObj.offsetHeight);
-        obj.style.left = left - (((mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth) * speed) + 'px';
-        obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px';
-    }
-
-    window.addEventListener("load", initParallax);
     window.addEventListener("resize", initParallax);
     /* ########################################### /hero parallax ############################################## */
 
     // Shuffle js filter and masonry
     const shuffleWrapper = document.querySelector('.shuffle-wrapper');
-    if(shuffleWrapper) {
+    if (shuffleWrapper) {
         var Shuffle = window.Shuffle;
 
-        const selectFilter = function(category) {
+        const selectFilter = function (category) {
             const cat = category === "" ? "all" : category;
             const el = document.querySelector(`input[name="shuffle-filter"][value="${cat}"]`)
-            if(el) {
+            if (el) {
                 el.checked = true;
                 el.parentElement.classList.add("active");
             }
@@ -93,31 +109,38 @@
                     myShuffle.filter(input.value);
                     window.location.hash = input.value
                 }
-               }
+            }
         })
 
-        document.addEventListener('lazyloaded', () =>  myShuffle.layout());
+        document.addEventListener('lazyloaded', () => myShuffle.layout());
     }
 
     // Animations on scroll
-    const callback = function(entries) {
+    const callback = function (entries) {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('is-visible');
-              } else {
+            } else {
                 entry.target.classList.remove('is-visible');
-              }
+            }
         });
-      };
-      
+    };
+
     const observer = new IntersectionObserver(callback, {
         rootMargin: '-120px 0px -80px',
         //threshold: 0.8
     });
-    
+
     const targets = document.querySelectorAll(".show-on-scroll");
-    targets.forEach(function(target) {
+    targets.forEach(function (target) {
         observer.observe(target);
     });
 
+    if (document.readyState === 'loading') {
+        window.addEventListener("DOMContentLoaded", initParallax);
+        window.addEventListener("DOMContentLoaded", foldHeader);
+    } else {
+        initParallax();
+        foldHeader();
+    }
 })();
